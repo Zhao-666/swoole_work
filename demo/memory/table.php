bf027bc5f32d4a19302118eb6e313126b7b269ed
Zhao-666 <2268699370@qq.com>
2018-05-23 07:57:33 +0800

    6-4 内存table的使用

---
+++ demo/memory/table.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Next
+ * Date: 2018/5/23
+ * Time: 7:51
+ */
+
+$table = new swoole_table(1024);
+
+$table->column('id', swoole_table::TYPE_INT, 4);
+$table->column('name', swoole_table::TYPE_STRING, 255);
+$table->column('age', swoole_table::TYPE_INT, 4);
+$table->create();
+
+$table->set('zzphp', ['id' => 1, 'name' => 'zzphp', 'age' => 20]);
+
+
+$ret = $table->get('zzphp');
+var_dump($ret);
