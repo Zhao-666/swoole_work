f3f41f6e8e1ce721c0470facafef34736324bf2a
Zhao-666 <2268699370@qq.com>
2018-05-23 08:02:04 +0800

    6-5 内存table使用

--- demo/memory/table.php
+++ demo/memory/table.php
@@ -14,7 +14,12 @@ $table->column('age', swoole_table::TYPE_INT, 4);
 $table->create();
 
 $table->set('zzphp', ['id' => 1, 'name' => 'zzphp', 'age' => 20]);
-
-
 $ret = $table->get('zzphp');
+$table['zzphp2']= [
+  'id'=>2,
+  'name'=>'zzphp2',
+  'age'=>30
+];
+
 var_dump($ret);
+var_dump($table['zzphp2']);
